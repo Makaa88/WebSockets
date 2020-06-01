86726100c2d8b842170a627ea65692628c4131f7
Makaa88 <maka6868@gmail.com>
2020-06-01 08:41:27 +0200

    simplify first example

--- src/ws/Endpoint.java
+++ src/ws/Endpoint.java
@@ -16,13 +16,10 @@ import java.util.concurrent.ConcurrentHashMap;
 @ServerEndpoint("/wsEndpoint")
 public class Endpoint
 {
-	private static final Set<Session> sessions = Collections.synchronizedSet(new HashSet<Session>());
 	
     @OnMessage
     public String handleMessage(String message)
     {
-    	if(message.equals("Opening message")) return getOpeningData();
-    	
     	JSONObject json = new JSONObject(message);
     	Person p = new Person();
     	p.setCity(json.getString("city"));
@@ -37,58 +34,17 @@ public class Endpoint
     	String messageToSend = "{\"people\": [";
     	messageToSend += message;
     	messageToSend += "]}";
-    	synchronized(sessions)
-    	{
-    		try {
-	    		for(Session session : sessions)
-	    		{
-	    			if(session.isOpen()) session.getBasicRemote().sendText(messageToSend);
-	    		}
-    		}
-    		catch(IOException ex)
-			{
-				System.out.println("Exception " + ex.toString());
-			}
-    	}
-        return message;
+    	
+        return messageToSend;
     }
 
     @OnOpen
     public void onOpen(Session session)
     {
-        sessions.add(session);
     }
 
     @OnClose
     public void onClose(Session session)
     {
-    	sessions.remove(session);
-    }
-    
-    private String getOpeningData() {
-    	 String json = "{\"people\": [";
-
-        Person person = new Person();
-        List<Person> personList = person.getPeople();
-        System.out.println(personList.size());
-        for(int i = 0; i < personList.size(); i++)
-        {
-            Person p = personList.get(i);
-            json += personToJsonString(p);
-            if(i <personList.size()-1) json+=",";
-        }
-
-        json += "]}";
-        System.out.println(json);
-        return json;
-    }
-    
-    private String personToJsonString(Person p)
-    {
-    
-    	String json = "{\"lname\": \"" + p.getLname() + "\",";
-        json += "\"fname\": \"" + p.getFname() + "\",";
-        json += "\"email\": \"" + p.getEmail() + "\"}";
-        return json;
     }
 }
